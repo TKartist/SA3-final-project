c538b4c557df49daa8934516f11aa4db3b087468
Francesco Caglianone <francescocaglianone@gmail.com>
2022-11-29 18:22:08 +0100

    first layout for create user page

---
+++ public/js/login.js
@@ -0,0 +1,10 @@
+
+
+function showPassword() {
+    var x = document.getElementById("myPassword");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+}
