2c86e6e9df99978070a629d5ed6a926299095b0e
Francesco Caglianone <francescocaglianone@gmail.com>
2022-11-30 16:05:53 +0100

    fixed the show password buttons

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,112 @@
 
 
-function showPassword() {
+// Show or don't show the password 
+function showPassword(pageName) {
 
-    var x = document.getElementById("myPassword");
-    var y = document.getElementById("showPasswordbtn");
+    // flag for button opacity
     var flag = false;
 
-    if (x.type === "password") {
-      x.type = "text";
-      flag = true;
+    // show or don't show the password
+    if(pageName === 'create'){
+        console.log("inside create")
+        var x = document.getElementById("createPassword");
+        var y = document.getElementById("createPassword2")
+        var z = document.getElementById("createPasswordbtn");
+
+        if (x.type === "password") {
+            x.type = "text";
+            y.type = "text";
+            flag = true;
+        } else {
+            x.type = "password";
+            y.type = "password";
+            flag = false;
+        }
+
     } else {
-      x.type = "password";
-      flag = false;
+        console.log("inside login");
+        var x = document.getElementById("loginPassword");
+        var z = document.getElementById("loginPasswordbtn");
+
+        if (x.type === "password") {
+            x.type = "text";
+            flag = true;
+        } else {
+            x.type = "password";
+            flag = false;
+        }
     }
 
+    // Button to show password opacity
     if(flag){
-        y.style.opacity = 0.5;
+        z.style.opacity = 0.5;
     } else {
-        y.style.opacity = 1;
+        z.style.opacity = 1;
+    }
+}
+
+
+
+
+
+
+
+// Move the submit button if the values are incorrect
+var myflag = 0;
+
+
+function mouseOver(){
+
+    // Get the inputs from the form
+    const email = document.forms['userCreateForm']['email'].value;
+    const username = document.forms['userCreateForm']['username'].value;
+    const password1 = document.forms['userCreateForm']['password'].value;
+    const password2 = document.forms['userCreateForm']['password-repeat'].value;
+
+    const emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(username == "" || !email.match(emailCheck) || password1 == "" || password2 == "" || password1 != password2 && myflag === 0) {
+        buttonMoveLeft();
+        console.log("moving left");
+        myflag = 1;
+        console.log(myflag);
+        return false;
     }
 
+    else if(username == "" || !email.match(emailCheck) || password1 == "" || password2 == "" || password1 != password2 && myflag === 1) {
+        buttonMoveRight();
+        console.log("moving right");
+        myflag = 2;
+        console.log(myflag);
+        return false;
+    }
+
+    else if(username == "" || !email.match(emailCheck) || password1 == "" || password2 == "" || password1 != password2 && myflag === 2) {
+        buttonMoveLeft();
+        console.log("moving left again");
+        myflag = 1;
+        return false;
+    }
+
+    else {
+        console.log("All inputs are correct");
+    }
+
+}
+
+
+function buttonMoveLeft(){
+    const button = document.getElementById('createUserbtn');
+    button.style.transform = 'translateX(-160%)';
+}
+
+function buttonMoveRight(){
+    const button = document.getElementById('createUserbtn');
+    button.style.transform = 'translateX(0%)';
+}
+
+function resetButton(){
+    const button = document.getElementById('createUserbtn');
+    console.log("button resetted")
+    button.style.transform = 'translateX(0%)';
 }
