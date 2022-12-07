b475726239597c29584bd4954304288a43c6ccec
Bonett0 <59030167+Bonett0@users.noreply.github.com>
2022-12-07 16:23:24 +0100

    lgoin function

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var User = require('../models/user')
+var User = require('../models/user');
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 mongoose.connect('mongodb://localhost:27017/login-app-db',{
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
+const KEY = 'wn8723yhufbvjeni23>"{+:983u9io19:">0jei(*&^%$dwnefi2981ijdwn';
 
 var router = express.Router();
 
@@ -15,8 +17,35 @@ var model = require ('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.redirect('/');
+  res.send('respond with a resource');
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('log-in', {});
+})
+
+router.post('/login', async(req, res)=> {
+
+  const { username, password } = req.body;
+
+  const user = await User.findOne({username}).lean(); // only json object 
+
+  if(!user){
+    return res.json({status: 'error', error : 'Invalid username/password'})
+  }
+
+  console.log('password = ' , user);
+
+  if(await bcrypt.compare(password, user.password_crypt)){
+    //username password combination is succeful
+
+    const token = jwt.sign({id : user._id, username: user.username}, KEY)   //visible data
+    
+    return res.json({status: 'ok', data: token})
+  }
+
+  res.json({status: 'error', error : 'Invalid username/password'})
+})
 
 router.get('/new', function(req, res, next) {
   res.render('sign-in', {});
@@ -26,6 +55,7 @@ router.post('/new', async(req, res)=> {
   console.log(req.body);
 
   //Hashing the password == algorithm bcrypt
+
   const {email, username, password} = req.body;
 
   //check email
@@ -34,11 +64,13 @@ router.post('/new', async(req, res)=> {
   }
  
   //check username
+
   if(!username || typeof username !== 'string'){
     return req.json({status: 'error',  error: 'Invalid Username'})
   }
 
   //check password
+
   if(!password || typeof password !== 'string'){
     return req.json({status: 'error',  error: 'Invalid Password'})
   }
@@ -68,8 +100,8 @@ router.post('/new', async(req, res)=> {
   res.json({status: 'ok'});
 })
 
-router.get('/login', function(req, res, next) {
-  res.render('log-in', {});
+router.get('/browse', function(req, res, next) {
+  res.render('browse', {});
 })
 
 module.exports = router;
