b0b2192a0cb8f3b54775ed303f11fcde6c947bf3
Bonett0 <59030167+Bonett0@users.noreply.github.com>
2022-12-11 16:20:18 +0100

    blocked every page with authentication function and send data through them

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 const verify = require('./verifyToken');
@@ -6,7 +7,7 @@ router.get('/index', function(req, res, next) {
   response = verify.check(req);
   if (response.status) {
     console.log(response.name)
-    res.render('index', { title: 'Index Page', filename: "unlocked" });
+    res.render('index', { title: 'Index Page', filename: "unlocked", name: response.name });
   } else {
     res.render('index', { title: 'Index Page', filename: "locked" });
   }
@@ -18,23 +19,26 @@ router.get('/', function(req, res, next) {
 
 router.get('/browse', verify.auth, function(req, res, next) {
   let name = req.userName;
-  res.render('browse', {});
+  console.log(name);
+  res.render('browse', {username : name});
 })
 
 
-router.get('/play', function(req, res, next) {
+router.get('/play',verify.auth ,function(req, res, next) {
   res.render('play', {});
 })
 
-router.get('/chat', function(req, res, next) {
+router.get('/chat',verify.auth ,function(req, res, next) {
   res.render('chat', {});
 
 })
-router.get('/learn', function(req, res, next) {
+router.get('/learn', verify.auth,function(req, res, next) {
   res.render('learn', {});
 })
 
-router.get('/room', function(req, res, next) {
-  res.render('room', {});
+router.get('/room',verify.auth ,function(req, res, next) {
+  let name = req.userName;
+  console.log(name);
+  res.render('room', {name : name});
 })
 module.exports = router;
