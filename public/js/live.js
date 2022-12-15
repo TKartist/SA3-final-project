90573df8a7ed5c72335d72f0daa4c02957e8dad8
Georgy Batyrev <87906732+BatyrGeo@users.noreply.github.com>
2022-12-15 17:30:43 +0100

    mor emore moooore

--- public/js/live.js
+++ public/js/live.js
@@ -19,14 +19,16 @@ document.getElementById("disconnect").addEventListener('click', () => {
 const messageContainer = document.getElementById('myForm');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
+
 socket.on('chat-message', data => {
-    // console.log(data);
+    console.log(data.message);
     appendMessage(`${data.name}: ${data.message}`);
 })
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
+    console.log("send message")
     appendMessage(`You: ${message}`);
     // to send information from the client to the server
     socket.emit('send-chat-message', message);
@@ -35,7 +37,8 @@ messageForm.addEventListener('submit', e => {
 })
 
 function appendMessage(message) {
-    const messageElement = document.createElement('div');
+    const messageElement = document.createElement('h6');
     messageElement.innerText = message;
-    messageContainer.append(messageElement);
+    console.log("received message")
+    messageContainer.appendChild(messageElement);
 }
