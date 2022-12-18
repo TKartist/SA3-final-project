a73a7dd7bafa1ac83dc519241ae2a7e7a0c0183a
TKartist <95535167+TKartist@users.noreply.github.com>
2022-12-18 01:38:16 +0100

    puzzles have been completed

--- public/js/levels.js
+++ public/js/levels.js
@@ -134,6 +134,9 @@ function solution() {
     if (running === 2 && moves[movePointer][0] === "74" && moves[movePointer][1] === "84") {
         board.set(moves[movePointer][1], "blackQueen");
         board.set(moves[movePointer][0], "empty");
+    } else if (running === 3 && moves[movePointer][0] === "25" && moves[movePointer][1] === "15") {
+        board.set(moves[movePointer][1], "whiteQueen");
+        board.set(moves[movePointer][0], "empty");
     } else {
         board.set(moves[movePointer][1], board.get(moves[movePointer][0]));
         board.set(moves[movePointer][0], "empty");
@@ -204,10 +207,20 @@ function d2(event) {
 function d3(event) {
     running = 3;
     document.querySelector("main").innerHTML = "";
+    let num = Math.floor(Math.random() * 3) + 1;
+    load();
+    if (num === 1) {
+        m41();
+    } else if (num === 2) {
+        m42();
+    } else {
+        m43();
+    }
     makeBoard();
 }
 
 function m21() {
+    atk = "white";
     board.set("11", "blackKing");
     board.set("12", "blackRook");
     board.set("13", "whiteHorse");
@@ -224,6 +237,7 @@ function m21() {
 }
 
 function m22() {
+    atk = "white";
     board.set("11", "blackRook");
     board.set("18", "blackRook");
     board.set("21", "blackKing");
@@ -246,6 +260,7 @@ function m22() {
 }
 
 function m23() {
+    atk = "white";
     board.set("15", "whiteRook");
     board.set("21", "blackPawn");
     board.set("23", "blackPawn");
@@ -265,6 +280,7 @@ function m23() {
 }
 
 function m31() {
+    atk = "white";
     board.set("26", "whiteHorse");
     board.set("28", "blackPawn");
     board.set("32", "whiteRook");
@@ -281,6 +297,7 @@ function m31() {
 }
 
 function m32() {
+    atk = "white";
     board.set("11", "blackRook");
     board.set("28", "blackPawn");
     board.set("54", "blackPawn");
@@ -295,6 +312,7 @@ function m32() {
 }
 
 function m33() {
+    atk = "white";
     board.set("13", "blackBishop");
     board.set("23", "whiteQueen");
     board.set("31", "blackRook");
@@ -311,4 +329,69 @@ function m33() {
     board.set("83", "whiteHorse");
     moves = [["23", "78"], ["77", "78"], ["76", "78"], ["46", "54"], ["78", "71"]];
 }
+
+function m41() {
+    atk = "white";
+    board.set("15", "whiteRook");
+    board.set("26", "blackPawn");
+    board.set("27", "blackPawn");
+    board.set("28", "blackKing");
+    board.set("38", "blackPawn");
+    board.set("44", "whiteBishop");
+    board.set("45", "blackPawn");
+    board.set("46", "whitePawn");
+    board.set("54", "blackBishop");
+    board.set("62", "whitePawn");
+    board.set("67", "whiteQueen");
+    board.set("68", "whitePawn");
+    board.set("71", "blackRook");
+    board.set("77", "whitePawn");
+    board.set("78", "whitePawn");
+    board.set("84", "blackQueen");
+    moves = [["67", "37"], ["26", "37"], ["44", "17"], ["28", "18"], ["17", "26"], ["18", "28"], ["46", "37"]];
+}
+
+function m42() {
+    atk = "black";
+    board.set("15", "whiteRook");
+    board.set("27", "blackRook");
+    board.set("31", "whiteKing");
+    board.set("32", "whitePawn");
+    board.set("33", "blackPawn");
+    board.set("34", "whitePawn");
+    board.set("38", "whitePawn");
+    board.set("41", "whitePawn");
+    board.set("43", "whiteQueen");
+    board.set("51", "blackPawn");
+    board.set("54", "blackQueen");
+    board.set("55", "whitePawn");
+    board.set("56", "blackPaw");
+    board.set("72", "blackPawn");
+    board.set("82", "blackKing");
+    moves = [["27", "21"], ["31", "21"], ["54", "27"], ["15", "25"], ["27", "25"], ["21", "12"], ["25", "22"]];
+}
+
+function m43() {
+    atk = "white";
+    board.set("15", "blackQueen");
+    board.set("22", "blackBishop");
+    board.set("25", "whitePawn");
+    board.set("26", "blackKing");
+    board.set("28", "blackPawn");
+    board.set("31", "blackRook");
+    board.set("37", "blackPawn");
+    board.set("45", "whiteQueen");
+    board.set("48", "blackHorse");
+    board.set("52", "blackPawn");
+    board.set("54", "whiteBishop");
+    board.set("66", "whitePawn");
+    board.set("72", "whitePawn");
+    board.set("77", "whitePawn");
+    board.set("78", "whitePawn");
+    board.set("83", "whiteRook");
+    board.set("85", "whiteRook");
+    board.set("88", "whiteKing");
+    moves = [["83", "33"], ["48", "67"], ["78", "67"], ["15", "33"], ["25", "15"], ["33", "15"], ["45", "27"]];
+}
+
 
