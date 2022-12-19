2107a5793d8a6d7710e94a5e6f2a465e8140d0c4
Georgy Batyrev <87906732+BatyrGeo@users.noreply.github.com>
2022-12-19 13:27:10 +0100

    push

--- public/js/about.js
+++ public/js/about.js
@@ -1,14 +1,10 @@
 function moveSlides() {
-    // Get the current position of the slides
-    var pos = parseInt(slides.style.left) || 0;
-  
-    // Move the slides to the left
-    pos -= 1;
-  
-    // Animate the transition
-    slides.style.transition = "left 0.1s linear";
-    slides.style.left = pos + "px";
-  }
-  
-  // Call the moveSlides function every 100 milliseconds
-  setInterval(moveSlides, 100);
+  var pos = parseInt(slides.style.left) || 0;
+
+  pos -= 1;
+
+  slides.style.transition = "left 0.1s linear";
+  slides.style.left = pos + "px";
+}
+
+setInterval(moveSlides, 100);
